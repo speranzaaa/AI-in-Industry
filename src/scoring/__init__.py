81431d84332e568be68c0440a9c2fd8a9e5794da
Angela Speranza <angela.speranza@studio.unibo.it>
2026-07-27 22:01:36 +0200

    Revert "Add cost model, threshold optimization, Occam baseline"
    This reverts commit d8bae02868e5873c7f5f8895670554e43ba76147.

--- src/scoring/__init__.py
+++ src/scoring/__init__.py
@@ -1,15 +1,3 @@
 from .scores import compute_score_llm, compute_score_kde, risk_level
-from .cost import (
-    binarize_labels,
-    confusion_counts,
-    cost_model,
-    optimize_threshold,
-    C_FN_DEFAULT,
-    C_FP_DEFAULT,
-)
 
-__all__ = [
-    "compute_score_llm", "compute_score_kde", "risk_level",
-    "binarize_labels", "confusion_counts", "cost_model",
-    "optimize_threshold", "C_FN_DEFAULT", "C_FP_DEFAULT",
-]
+__all__ = ["compute_score_llm", "compute_score_kde", "risk_level"]
